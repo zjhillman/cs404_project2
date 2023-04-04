df5860db3d1939ea08dfe6bfbb37665243966f84
Zachary Hillman <zjhillman@outlook.com>
2023-04-03 22:28:01 -0500

    Added startRegistry()

--- VotingServer.java
+++ VotingServer.java
@@ -1,4 +1,7 @@
 import java.io.*;
+import java.rmi.RemoteException;
+import java.rmi.registry.Registry;
+import java.rmi.registry.LocateRegistry;
 
 /*
  * This class is the server to host the pull using java
@@ -6,13 +9,13 @@ import java.io.*;
  * Use VotingServer [port] [InetAddress] []
  */
 public class VotingServer {
+    private static boolean DEBUG = true;
     private BufferedReader stdIn = new BufferedReader(
-        new InputStreamReader(System.in)
-    );
+        new InputStreamReader(System.in) );
     private static int port;
     private static String registry;
-
-    public static void main (String args[]) {
+    
+    private static void parseArgs (String args[]) { 
         // process flags
         int i = 0;
         while (i < args.length && args[i].startsWith("-")) {
@@ -33,9 +36,35 @@ public class VotingServer {
                     break;
             }
         }
-
-        System.out.println("Port: " + port);
-        System.out.println("IP Address: " + registry);
-        System.out.println("done");
     }
+
+    private static void startRegistry () throws RemoteException {
+        try {
+            Registry registry = LocateRegistry.getRegistry(port);
+            registry.list();
+        } catch (RemoteException re) {
+            System.out.println("Cannot find registry at port " + port);
+            Registry registry = LocateRegistry.createRegistry(port);
+            System.out.println("Registry created at " + port);
+        }
+        
+    }
+
+    public static void main (String args[]) {
+        parseArgs(args);
+        if (DEBUG) {
+             System.out.println("Port: " + port);
+            System.out.println("IP Address: " + registry);
+            System.out.println("done");
+        }
+
+        try {
+            VotingImplementation vi = new VotingImplementation();
+        }
+        catch (RemoteException re) {
+            re.getCause();
+            re.printStackTrace();
+        }
+
+    } // end main
 }
