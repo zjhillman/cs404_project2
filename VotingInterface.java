dfb59259ecf60508e051e844678be6de7b65af17
Zachary Hillman <zjhillman@outlook.com>
2023-04-03 20:49:32 -0500

    init implementation & interface

---
+++ VotingInterface.java
@@ -0,0 +1,8 @@
+import java.rmi.*;
+
+public interface VotingInterface extends Remote {
+    /*
+     * TODO: Add methods for implementation
+     */
+    public String sayHello (String phrase) throws java.rmi.RemoteException;
+}
