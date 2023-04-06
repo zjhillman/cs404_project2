052853a42243e94bb2f7108c8cd19e046212f54f
Zachary Hillman <zjhillman@outlook.com>
2023-04-05 22:22:52 -0500

    added name flag, loop on poll response
    v0.3.6

--- VotingClient.java
+++ VotingClient.java
@@ -1,12 +1,17 @@
 import java.io.*;
 import java.rmi.*;
+import java.util.ArrayList;
 
+/*
+ * VotingClient
+ * v0.3.6
+ */
 public class VotingClient {
-    private static boolean DEBUG = true;
+    private static boolean DEBUG = false;
     private static int rmiPort;
     private static String hostName;
-    private static String registryURL;
     private static String usersName;
+    private static String registryURL;
     private static BufferedReader stdIn = new BufferedReader(
         new InputStreamReader(System.in)
     );
@@ -14,36 +19,41 @@ public class VotingClient {
     private static void parseArgs (String args[]) { 
         // process flags
         int i = 0;
-        boolean pSet = false, hSet = false;
+        boolean pSet = false, hSet = false, nSet = false;
         while (i < args.length && args[i].startsWith("-")) {
             String arg = args[i];
             char flag = arg.charAt(1);
             
-            try {
-                switch (flag) {
-                    case 'p':
-                        rmiPort = Integer.parseInt(args[i+1]);
-                        i += 2;
-                        pSet = true;
-                        break;
-                    case 'h':
-                        hostName = args[i + 1];
-                        i += 2;
-                        hSet = true;
-                        break;
-                    case 'n':
-                        usersName = stdIn.readLine();
-                    default:
-                        i++;
-                        break;
-                }
-            } catch (IOException ioe) {
-                ioe.printStackTrace();
+            switch (flag) {
+                case 'p':
+                    rmiPort = Integer.parseInt(args[i+1]);
+                    i += 2;
+                    pSet = true;
+                    break;
+                case 'h':
+                    hostName = args[i + 1];
+                    i += 2;
+                    hSet = true;
+                    break;
+                case 'n':
+                    usersName = args[i + 1];
+                    i += 2;
+                    nSet = true;
+                    break;
+                default:
+                    i++;
+                    break;
             }
         }
 
         try {
-            if (!pSet) {
+            if (!nSet) {
+                System.out.println("What is your name?");
+                String in = stdIn.readLine();
+                usersName = in;
+                nSet = true;
+            }
+             if (!pSet) {
                 System.out.print("Please enter a port number: ");
                 String in = stdIn.readLine();
                 rmiPort = Integer.parseInt(in);
@@ -92,30 +102,53 @@ public class VotingClient {
     public static void main (String args[]) {
         parseArgs(args);
         if (DEBUG) {
-            System.out.println("Port: " + rmiPort);
+            System.out.println("Name: ");
             System.out.println("Host Name: " + hostName);
-            System.out.println("cla parsed");
+            System.out.println("Port Number: " + rmiPort);
+            System.out.println("--CLA Parsed--");
         }
 
         try {
+            ArrayList <String> yesCommands = new ArrayList<String>();
+            yesCommands.add("yes");
+            yesCommands.add("yeah");
+            yesCommands.add("y");
+            yesCommands.add("yuh");
+            ArrayList <String> noCommands = new ArrayList<String>();
+            noCommands.add("no");
+            noCommands.add("n");
+            noCommands.add("nah");
+            noCommands.add("no thanks");
+
             registryURL = "rmi://" + hostName + ":" + rmiPort + "/voting";
             VotingInterface vi = (VotingInterface) Naming.lookup(registryURL);
             System.out.println("Established connection to " + registryURL);
 
             // greet
-            System.out.println("What is your name?");
-            usersName = stdIn.readLine();
-            String message = vi.sayHello(name);
+            String message = vi.sayHello(usersName);
             System.out.println("\n" + message + ",");
 
             // state topic, ask to cast ballot
             message = vi.getTopic();
-            System.out.println("Today's topic is: '" + message +"'");
-            System.out.println("Do you wish to participate in the poll?");
-            String response = stdIn.readLine();
-            if (response.toLowerCase().equals("yes"))
-                poll(vi);
+            System.out.println("Today's topic is '" + message +"'");
 
+            boolean properResponse = true;
+            do {
+                System.out.println("Do you wish to participate in the poll?");
+                String response = stdIn.readLine();
+                if (yesCommands.contains(response)) {
+                    poll(vi);
+                    properResponse = true;
+                }
+                else if (noCommands.contains(response)) {
+                    System.out.println("\nYou chose not to particpate in the poll");
+                    properResponse = true;
+                }
+                else {
+                    System.out.println("Command not understood, please try again");
+                    properResponse = false;
+                }
+            } while (!properResponse);
 
             // loop to view results until client disconnects
             while (true) {
