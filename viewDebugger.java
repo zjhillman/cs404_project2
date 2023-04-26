2a7dcdb1fce53e81a15189b022bf433893abcb8e
Zachary Hillman <zjhillman@outlook.com>
2023-04-26 00:21:48 -0500

    removal of view testing from main

--- viewDebugger.java
+++
@@ -1,9 +0,0 @@
-import views.ClientView;
-
-public class viewDebugger {
-    public static void main (String args[]) {
-        ClientView view = new ClientView();
-        view.setVisible(true);
-        view.showArguements();
-    }
-}
